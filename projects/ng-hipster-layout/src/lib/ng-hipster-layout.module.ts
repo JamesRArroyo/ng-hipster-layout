197187bf48d7f7a54a9803c3546b32fcb53399bd
Jimmy Arroyo <jamesarroyo5@gmail.com>
2018-08-14 22:40:40 -0600

    layout displays side nav

--- projects/ng-hipster-layout/src/lib/ng-hipster-layout.module.ts
+++ projects/ng-hipster-layout/src/lib/ng-hipster-layout.module.ts
@@ -1,12 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NgHipsterLayoutComponent } from './ng-hipster-layout.component';
+import { ButtonIconComponent } from './button-icon/button-icon.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
-  imports: [
-    RouterModule
+  imports: [BrowserModule, RouterModule],
+  declarations: [
+    NgHipsterLayoutComponent,
+    ButtonIconComponent,
+    SidenavComponent,
   ],
-  declarations: [NgHipsterLayoutComponent],
-  exports: [NgHipsterLayoutComponent]
+  exports: [NgHipsterLayoutComponent],
 })
-export class NgHipsterLayoutModule { }
+export class NgHipsterLayoutModule {}
