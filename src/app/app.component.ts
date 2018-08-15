7f28b9cf1eb1eb7534c76e87807922a07bc64827
Jimmy Arroyo <jamesarroyo5@gmail.com>
2018-08-14 23:22:53 -0600

    app component now passes in sample sidenav links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgHipsterLayoutService, SidenavLink } from 'ng-hipster-layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-hipster-layout-app';
+  sidenavLinks: SidenavLink[] = [
+    { title: 'Home', route: '/home', icon: 'fas fa-home' },
+    { title: 'Dashboard', route: '/dashboard', icon: 'fas fa-tachometer-alt' },
+    { title: 'Reports', route: '/reports', icon: 'fas fa-chart-pie' },
+  ];
+
+  constructor(private layoutService: NgHipsterLayoutService) {
+    layoutService.setSidenavLinks(this.sidenavLinks);
+  }
 }
