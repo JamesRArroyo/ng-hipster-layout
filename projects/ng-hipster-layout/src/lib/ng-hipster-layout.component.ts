5fcc610bfec2d15c0ab76b4d69b91fb3713e17b3
Jimmy Arroyo <jamesarroyo5@gmail.com>
2018-08-14 21:09:16 -0600

    Changed css to scss

--- projects/ng-hipster-layout/src/lib/ng-hipster-layout.component.ts
+++ projects/ng-hipster-layout/src/lib/ng-hipster-layout.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'abox-ng-hipster-layout',
   templateUrl: './ng-hipster-layout.component.html',
-  styleUrls: ['./ng-hipster-layout.component.css']
+  styleUrls: ['./ng-hipster-layout.component.scss']
 })
 export class NgHipsterLayoutComponent implements OnInit {
 
