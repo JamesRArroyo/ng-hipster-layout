093ce393abe6626ce70e0793f33769986e80ee53
Jimmy Arroyo <jamesarroyo5@gmail.com>
2018-08-14 23:21:38 -0600

    Added function to set sidenav links in the layout service

--- projects/ng-hipster-layout/src/lib/ng-hipster-layout.service.ts
+++ projects/ng-hipster-layout/src/lib/ng-hipster-layout.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
+export interface SidenavLink {
+  route: string;
+  title?: string;
+  icon?: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgHipsterLayoutService {
+  private sidenavLinksBehaviorSubject = new BehaviorSubject<SidenavLink[]>([]);
+  sidenavLinks = this.sidenavLinksBehaviorSubject.asObservable();
 
-  constructor() { }
+  constructor() {}
+
+  setSidenavLinks(links: SidenavLink[]): Promise<void> {
+    this.sidenavLinksBehaviorSubject.next(links);
+    return Promise.resolve();
+  }
 }
