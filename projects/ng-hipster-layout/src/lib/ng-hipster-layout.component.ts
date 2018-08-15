cfa4e7e5704dc8c55db6907e0e89e8b715542302
Jimmy Arroyo <jamesarroyo5@gmail.com>
2018-08-14 23:22:14 -0600

    The layout component now subscribes to the sidenav links and passes them to the sidenav component

--- projects/ng-hipster-layout/src/lib/ng-hipster-layout.component.ts
+++ projects/ng-hipster-layout/src/lib/ng-hipster-layout.component.ts
@@ -1,15 +1,20 @@
+import {
+  SidenavLink,
+  NgHipsterLayoutService,
+} from './ng-hipster-layout.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'abox-ng-hipster-layout',
   templateUrl: './ng-hipster-layout.component.html',
-  styleUrls: ['./ng-hipster-layout.component.scss']
+  styleUrls: ['./ng-hipster-layout.component.scss'],
 })
 export class NgHipsterLayoutComponent implements OnInit {
-
-  constructor() { }
+  links$: Observable<SidenavLink[]>;
+  constructor(private layoutService: NgHipsterLayoutService) {}
 
   ngOnInit() {
+    this.links$ = this.layoutService.sidenavLinks;
   }
-
 }
